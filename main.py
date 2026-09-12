14504cd18a6af3a29cc54f36fd4f3c30006e9991
Sadek <186719556+SadekAlSaabari@users.noreply.github.com>
2026-09-13 00:59:53 +0100

    added logging functionality and a dockerfile and .dockerignore for containerising the application

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from agents.inventory_agent import InventoryAgent
 from agents.manager_agent import ManagerAgent
 from agents.planner_agent import PlannerAgent
 from models import InventoryInput, InventoryResponse, DietInput, DietResponse, ManagerInput, ManagerResponse, RecipeGroupResponse, RecipeResponse, RecipePlanInput, RecipeRecommendInput
+from app.logging import get_logger
+import uvicorn
 
 # FastAPI istance defining routes and handling HTTP requests
 app = FastAPI(title="AI Diet and Meal Planner")
@@ -13,6 +15,9 @@ inventory_agent = InventoryAgent()
 diet_agent = DietAgent()
 manager_agent = ManagerAgent()
 planner_agent = PlannerAgent()
+
+# initialise logger
+logger = get_logger(name="app")
 
 # root endpoint returning a success message indicating the server is activated
 @app.get("/")
@@ -34,17 +39,23 @@ def diet_request(user_input: DietInput) -> DietResponse:
 # ask endpoint taking a list of items and preferred diet, then using both the inventory and diet agents to return a list of usable items, items compatible with the diet requirement and a list of 5 recipe ideas
 @app.post("/ask", response_model=ManagerResponse)
 def manager_request(user_input: ManagerInput) -> ManagerResponse:
+    logger.info("Received /ask request: items=%s, diet=%s", user_input.items, user_input.diet)
     result = manager_agent.run(user_input=user_input)
+    logger.info("/ask response: suggestions=%s", result.suggestions)
     return result
 
 # plan endpoint taking a dish and returning a step by step recipe including ingredients needed and instructions on how to prepare the dish
 @app.post("/plan", response_model=RecipeResponse)
 def planner_request(user_input: RecipePlanInput) -> RecipeResponse:
+    logger.info("Received /plan request: base_recipe=%s", user_input.base_recipe)
     result = planner_agent.planRecipe(user_input=user_input)
+    logger.info("/plan response: title=%s", result.title)
     return result
 
 # recommend endpoint taking a list of items and dietary preference, and returning a list of recipe ideas including step by step instructions for each
 @app.post("/recommend", response_model=RecipeGroupResponse)
 def planner_recommend(user_input: RecipeRecommendInput) -> RecipeGroupResponse:
+    logger.info("Received /recommend request: items=%s, diet=%d, recipe_count=%r", user_input.items, user_input.diet, user_input.recipe_count)
     result = planner_agent.recommend(user_input=user_input)
+    logger.info("/recommend response: number_of_recipes=%s", len(result.recipes))
     return result
